15eb64a6b8f66f6a39c8f54bac98c96b7ad0c10c
acorbacho <aaroncorbachovieites@gmail.com>
2022-05-24 08:16:02 +0200

    Archivo PHP de conexión

---
+++ src/php/conection.php
@@ -0,0 +1,7 @@
+<?php
+// Archivo de conexión con los datos de la db, comprueba la conexión y lanza mensaje de error en caso de problema de autenticación.
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "restaurant";
+$conection = mysqli_connect($server, $user, $pass, $db) or die("Error al conectar con la db." . mysqli_connect_error());
