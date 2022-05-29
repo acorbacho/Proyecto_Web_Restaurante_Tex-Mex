95a6799c05fea13dacedb30a39e0f2a7cf6dbfd6
acorbacho <aaroncorbachovieites@gmail.com>
2022-05-29 02:42:17 +0200

    Corrección de errores

--- src/php/signup.php
+++ src/php/signup.php
@@ -3,21 +3,21 @@
 include("connection.php");
 //Recibimos los datos del formulario y los almacenamos en variables.
 $username = $_POST["username"];
-$password = $_POST["password"];
 $email = $_POST["email"];
+$password = $_POST["password"];
 $admin = false;
 //Verificamos que todos los campos son cubiertos.
 if ($user != NULL and $password != NULL and $email != NULL) {
   //Comprobamos que el usuario no está registrado
   $username_select_query = "SELECT * FROM users WHERE username = '$username'";
-  $username_verify = mysqli_query($conection, $username_select_query);
+  $username_verify = mysqli_query($connection, $username_select_query);
   if (mysqli_num_rows($username_verify) > 0) {
     echo "Usuario ya registrado";
     exit;
   }
   //Registramos al usuario.
   $signup_insert_query = "INSERT INTO users(username, password, email, admin) VALUES ('$username','$password','$email','$admin')";
-  $signup_insert = mysqli_query($conection, $signup_insert_query);
+  $signup_insert = mysqli_query($connection, $signup_insert_query);
   //Comprobamos que no haya errores al registrarse y redirigimos.
   if (!$signup_insert)
     echo "Error al registrarse";
@@ -26,4 +26,4 @@ if ($user != NULL and $password != NULL and $email != NULL) {
 } else
   echo "Error de campos";
 //Cierre de conexión.
-mysqli_close($conexion);
+mysqli_close($connection);
